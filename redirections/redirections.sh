3c0e8c773fab9c42ec4913d0da776dfa03a6e1e7
Yoav Klein <yoavklein25@gmail.com>
2022-07-28 11:20:59 +0300

    redirections

---
+++ redirections/redirections.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+simple_stdout() {
+    # let's start with a simple redirection of stdout
+    # this will create the "log.txt" file
+
+    echo "ERROR: something happended" > log.txt
+
+}
+
+appending() {
+    # appending to a file is done with >>
+    echo "ERROR: still a probelm" >> log.txt
+}
+
+redirection_order() {
+    # redirection order is significant
+    # remember: redirection is making a copy of the file descriptor
+
+    # this  command generates stderr messages
+    #$ ls not_exist_file redirections.sh
+
+    # if we want to redirect stderr to stdout and then all that to a file
+    # this will not work
+    ls not_exist_file redirections.sh 2>&1 > log.txt
+    # this seems counter-intuitive, but remember, redirecting is making a copy of the file descriptor
+    # so we make a copy of FD 1 to FD 2, which in this point is still the terminal, 
+    # and only then redirect FD 1 to log.txt
+
+    # this will work
+    ls not_exist_file redirections.sh > log.txt 2>&1
+}
+
+redirection_location() {
+    # redirections can be before or after the command
+
+    # may be before
+    > log.txt 2>&1 ls not_exist_file redirections.sh 
+    echo "===="
+    cat log.txt
+    
+    # may be after
+    ls not_exist_file redirections.sh > log.txt 2>&1
+    echo "===="
+    cat log.txt
+
+    # may be in both
+    > log.txt ls not_exist_file redirections.sh 2>&1
+    echo "===="
+    cat log.txt
+}
+
+# simple
+# appending
+#redirection_order
+redirection_location
+
+#clean
+
